cfe153df4d2721de23ea7d43857d59378451f6ab
quoth-the-dev <andrewgorman.dev@gmail.com>
2024-03-10 15:05:55 +0100

    Day 2p1

--- day_2/2.py
+++ day_2/2.py
@@ -2,20 +2,24 @@ import time
 import pathlib
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def calculate_final_position_depth_aim_product(file_path: str) -> int:
+    instructions = _parse_instructions(file=file_path)
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for instruciton in instructions:
+        pass
 
 
-def _RENAME_FUNC(file: str):
+def _parse_instructions(file: str) -> list[tuple[str, int]]:
     with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        lines = puzzle_input.read().splitlines()
+        return [((line.split()[0], int(line.split()[1]))) for line in lines]
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(calculate_final_position_depth_aim_product("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 start = time.perf_counter()
-print(RENAME_FUNC("input.txt"))
+print(calculate_final_position_depth_aim_product("input.txt"))
 print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
