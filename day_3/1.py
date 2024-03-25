74edbd8e580cd29800a8e4341695eb03013a5ed5
quoth-the-dev <andrewgorman.dev@gmail.com>
2024-03-25 21:26:25 +0100

    Begin using pd.df approach

--- day_3/1.py
+++ day_3/1.py
@@ -1,21 +1,31 @@
 import time
 import pathlib
+import pandas
+
+# initialize list of lists
+data = [['tom', 10], ['nick', 15], ['juli', 14]]
+ 
+# Create the pandas DataFrame
+df = pandas.DataFrame(data, columns=['Name', 'Age'])
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def calculate_power_consumption(file_path: str):
+    diagnostic = _parse_diagnostic(file=file_path)
+    print(diagnostic)
 
 
-def _RENAME_FUNC(file: str):
+
+
+def _parse_diagnostic(file: str):
     with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
-
+        lines = puzzle_input.read().splitlines()
+        grid=[list(map(int, line)) for line in lines]
+        print(f"{grid=}")
+        return pandas.DataFrame(grid, columns=[i + 1 for i in range(len(lines[0]))])
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(calculate_power_consumption("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
-start = time.perf_counter()
-print(RENAME_FUNC("input.txt"))
-print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+# start = time.perf_counter()
+# print(calculate_power_consumption("input.txt"))
+# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
